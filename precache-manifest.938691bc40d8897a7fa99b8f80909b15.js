45ba3f7f7455fb7caf86b23656b0638af1fdc55e
imj <imj.programming.2013@gmail.com>
2020-11-23 13:32:07 +1100

    Updates

---
+++ precache-manifest.938691bc40d8897a7fa99b8f80909b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb22269dbc8ac920f17b29903a60d942",
+    "url": "/react-project-1/index.html"
+  },
+  {
+    "revision": "b8aca79d2d09b938a220",
+    "url": "/react-project-1/static/css/main.0d312782.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/react-project-1/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-project-1/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8aca79d2d09b938a220",
+    "url": "/react-project-1/static/js/main.04fdf820.chunk.js"
+  },
+  {
+    "revision": "ccfbfa8d58ec967cae5f",
+    "url": "/react-project-1/static/js/runtime-main.35fc640d.js"
+  }
+]);
